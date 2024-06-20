e342b4771080965666cc2917978d7d8982c99e74
Mike <126797494+miketreyu@users.noreply.github.com>
2024-06-20 18:52:16 +0200

    Add files via upload
    update

--- PÁGINA WEB RAISE YOUR VOICE SAINT LUCIA/scripts.js
+++ PÁGINA WEB RAISE YOUR VOICE SAINT LUCIA/scripts.js
@@ -11,3 +11,18 @@ function showQuestionnaire() {
     document.getElementById('domestic-violence-form').style.display = 'none';
     document.getElementById('domestic-violence-questionnaire').style.display = 'block';
 }
+
+function showQuestionnaire() {
+    document.getElementById('sexual-violence-form').style.display = 'none';
+    document.getElementById('sexual-violence-questionnaire').style.display = 'block';
+}
+
+function showQuestionnaire() {
+    document.getElementById('divorce-form').style.display = 'none';
+    document.getElementById('divorce-questionnaire').style.display = 'block';
+}
+
+function showQuestionnaire() {
+    document.getElementById('child-maintenance-form').style.display = 'none';
+    document.getElementById('child-maintenance-questionnaire').style.display = 'block';
+}
